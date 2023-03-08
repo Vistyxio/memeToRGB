6847d90fea40b3fd4c2000e5f295df148908c092
Vistyxio <56958893+Vistyxio@users.noreply.github.com>
2023-03-08 13:50:11 +0800

    Add files via upload

---
+++ 图片RGB化_1_2_1.cpp
@@ -0,0 +1,406 @@
+﻿#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<string>
+#include<ctime>
+#include<thread>
+#include<cstdlib>
+#include<fstream>
+#include<cstdio>
+#include<opencv2/opencv.hpp>
+//CODER BY Vistyxio POWERD BY VS2019
+using namespace std;
+using namespace cv;
+
+string str1 = "final\\final";
+string num;
+int n = 1, chann;
+string str2 = ".png";
+string name;
+vector<Mat> sequ;
+
+void myRGB(Mat& dst, vector<Point> vec, int a, int b) {
+	for (register int i = 0; i <= 255; i++) {
+		for (auto v : vec) {
+			if (chann == 4) {
+				dst.at<Vec4b>(v.x, v.y)[a] = 255 - i;
+				dst.at<Vec4b>(v.x, v.y)[b] = i;
+			} else {
+				dst.at<Vec3b>(v.x, v.y)[a] = 255 - i;
+				dst.at<Vec3b>(v.x, v.y)[b] = i;
+			}
+			
+		}
+		num = to_string(n);
+		name = str1 + num + str2;
+		imwrite(name, dst);
+		cout << setprecision(2) << fixed << double(100.0 * double(n) / 768.0) << "%" << endl;
+		n++;
+	}
+}
+
+void clearr(Mat& dst, vector<Point> vec) {
+	for (auto v : vec) {
+		if (chann == 4) {
+			dst.at<Vec4b>(v.x, v.y)[0] = 0;
+			dst.at<Vec4b>(v.x, v.y)[1] = 0;
+			dst.at<Vec4b>(v.x, v.y)[2] = 0;
+			dst.at<Vec4b>(v.x, v.y)[3] = 255;
+		} else {
+			dst.at<Vec3b>(v.x, v.y)[0] = 0;
+			dst.at<Vec3b>(v.x, v.y)[1] = 0;
+			dst.at<Vec3b>(v.x, v.y)[2] = 0;
+		}
+		
+	}
+}
+
+void shold() {
+	string str;
+	Mat src = sequ.back();
+	Mat shold_src;
+	while (1) {
+		cout << "输入a阈值以使用THRESH_BINARY进行二值化" << endl;
+		cout << "输入b阈值以使用THRESH_BINARY_INV进行二值化" << endl;
+		cout << "通俗理解a是让黑色更黑 白色更白 b是相反 可以理解成a的反色" << endl;
+		cout << "例如a120" << endl;
+		cout << endl << "范围0-255 一般在100-220" << endl;
+		cout << "调节完成输入ok" << endl;
+		cout << "输入exit退出 不保留修改" << endl;
+		cin >> str;
+		if (str == "ok") break;
+		if (str == "exit") {
+			destroyAllWindows();
+			return;
+		}
+		if (str.substr(0, 1) != "a" && str.substr(0, 1) != "b") continue;
+		if (str.substr(0, 1) == "a") {
+			threshold(src, shold_src, saturate_cast<uchar>(stoi(str.substr(1))), 255, THRESH_BINARY);
+		} else if (str.substr(0, 1) == "b") {
+			threshold(src, shold_src, saturate_cast<uchar>(stoi(str.substr(1))), 255, THRESH_BINARY_INV);
+		}
+		imshow("threshold", shold_src);
+		waitKey(5);
+	}
+	sequ.emplace_back(shold_src);
+	destroyAllWindows();
+}
+
+void median() {
+	string str;
+	Mat src = sequ.back();
+	Mat median_src;
+	while (1) {
+		cout << endl << "输入数字进行中值滤波 大于0 必须为奇数" << endl;
+		cout << "一般在10以内" << endl;
+		cout << "调节完成输入ok" << endl;
+		cout << "输入exit退出 不保留修改" << endl;
+		cin >> str;
+		if (str == "ok") break;
+		if (str == "exit") {
+			destroyAllWindows();
+			return;
+		}
+		medianBlur(src, median_src, stoi(str));
+		imshow("medianBlur", median_src);
+		waitKey(5);
+	}
+	sequ.emplace_back(median_src);
+	destroyAllWindows();
+}
+
+void erosion() {
+	string str;
+	Mat src = sequ.back();
+	Mat erode_src;
+	Mat kernel;
+	while (1) {
+		cout << endl << "输入数字定义内核进行腐蚀操作 大于0 必须为奇数" << endl;
+		cout << "一般在10以内" << endl;
+		cout << "调节完成输入ok" << endl;
+		cout << "输入exit退出 不保留修改" << endl;
+		cin >> str;
+		if (str == "ok") break;
+		if (str == "exit") {
+			destroyAllWindows();
+			return;
+		}
+		kernel = getStructuringElement(MORPH_RECT, Size(stoi(str), stoi(str)));
+		erode(src, erode_src, kernel);
+		imshow("erode", erode_src);
+		waitKey(5);
+	}
+	sequ.emplace_back(erode_src);
+	destroyAllWindows();
+}
+
+void dilation() {
+	string str;
+	Mat src = sequ.back();
+	Mat dilate_src;
+	Mat kernel;
+	while (1) {
+		cout << endl << "输入数字定义内核进行膨胀操作 大于0 必须为奇数" << endl;
+		cout << "一般在10以内" << endl;
+		cout << "调节完成输入ok" << endl;
+		cout << "输入exit退出 不保留修改" << endl;
+		cin >> str;
+		if (str == "ok") break;
+		if (str == "exit") {
+			destroyAllWindows();
+			return;
+		}
+		kernel = getStructuringElement(MORPH_RECT, Size(stoi(str), stoi(str)));
+		dilate(src, dilate_src, kernel);
+		imshow("dilate", dilate_src);
+		waitKey(5);
+	}
+	sequ.emplace_back(dilate_src);
+	destroyAllWindows();
+}
+
+void open() {
+	string str;
+	Mat src = sequ.back();
+	Mat open_src;
+	Mat kernel;
+	while (1) {
+		cout << endl << "输入数字定义内核进行开操作 大于0 必须为奇数" << endl;
+		cout << "可以消除白色的小点 去除小的干扰块" << endl;
+		cout << "一般在10以内" << endl;
+		cout << "调节完成输入ok" << endl;
+		cout << "输入exit退出 不保留修改" << endl;
+		cin >> str;
+		if (str == "ok") break;
+		if (str == "exit") {
+			destroyAllWindows();
+			return;
+		}
+		kernel = getStructuringElement(MORPH_RECT, Size(stoi(str), stoi(str)));
+		morphologyEx(src, open_src, MORPH_OPEN, kernel);
+		imshow("MORPH_OPEN", open_src);
+		waitKey(5);
+	}
+	sequ.emplace_back(open_src);
+	destroyAllWindows();
+}
+
+void close() {
+	string str;
+	Mat src = sequ.back();
+	Mat close_src;
+	Mat kernel;
+	while (1) {
+		cout << endl << "输入数字定义内核进行闭操作 大于0 必须为奇数" << endl;
+		cout << "可以消除黑色的小块 填充闭合区域" << endl;
+		cout << "一般在10以内" << endl;
+		cout << "调节完成输入ok" << endl;
+		cout << "输入exit退出 不保留修改" << endl;
+		cin >> str;
+		if (str == "ok") break;
+		if (str == "exit") {
+			destroyAllWindows();
+			return;
+		}
+		kernel = getStructuringElement(MORPH_RECT, Size(stoi(str), stoi(str)));
+		morphologyEx(src, close_src, MORPH_CLOSE, kernel);
+		imshow("MORPH_CLOSE", close_src);
+		waitKey(5);
+	}
+	sequ.emplace_back(close_src);
+	destroyAllWindows();
+}
+
+void gradient() {
+	string str;
+	Mat src = sequ.back();
+	Mat gradient_src;
+	Mat kernel;
+	while (1) {
+		cout << endl << "输入数字定义内核进行梯度操作 大于0 必须为奇数" << endl;
+		cout << "梯度就是膨胀和腐蚀的差 可用来保留轮廓" << endl;
+		cout << "一般在10以内" << endl;
+		cout << "调节完成输入ok" << endl;
+		cout << "输入exit退出 不保留修改" << endl;
+		cin >> str;
+		if (str == "ok") break;
+		if (str == "exit") {
+			destroyAllWindows();
+			return;
+		}
+		kernel = getStructuringElement(MORPH_RECT, Size(stoi(str), stoi(str)));
+		morphologyEx(src, gradient_src, MORPH_GRADIENT, kernel);
+		imshow("MORPH_GRADIENT", gradient_src);
+		waitKey(5);
+	}
+	sequ.emplace_back(gradient_src);
+	destroyAllWindows();
+}
+
+void canny() {
+	string str;
+	int n, m;
+	Mat src = sequ.back();
+	Mat canny_src;
+	while (1) {
+		cout << endl << "输入三个数字进行Canny边缘检测 大于0" << endl;
+		cout << "可以对输入图像进行边缘检测" << endl;
+		cout << "低于阈值1的像素点会被认为不是边缘" << endl;
+		cout << "高于阈值2的像素点会被认为是边缘" << endl;
+		cout << "在阈值1和阈值2之间的像素点,若与第2步得到的边缘像素点相邻，则被认为是边缘，否则被认为不是边缘" << endl;
+		cout << "调节完成输入ok" << endl;
+		cout << "输入exit退出 不保留修改" << endl;
+		cout << "先输入阈值1" << endl;
+		cin >> str;
+		if (str == "ok") break;
+		if (str == "exit") {
+			destroyAllWindows();
+			return;
+		}
+		cout << "再输入阈值2" << endl;
+		cin >> n;
+		cout << "再输入内核大小 一般是3" << endl;
+		cin >> m;
+		Canny(src, canny_src, stoi(str), n, m, false);
+		imshow("Canny", canny_src);
+		waitKey(5);
+	}
+	sequ.emplace_back(canny_src);
+	destroyAllWindows();
+}
+
+int main() {
+	system("title 图片RGB化生成器");
+	Mat src, dst_src, dst, element = getStructuringElement(MORPH_RECT, Size(3, 3));
+	string filePath, n, sttr;
+	int a;
+	vector<Point> vec;
+	cout << "RGB图片制作器 可以把图片变成RGB变化的gif" << endl;
+	cout << "建议先阅读readme.txt了解几种基本的操作" << endl;
+	cout << "输入路径或拖动图片到此" << endl;
+	cout << "新增功能:可输入或拖入由魔棒工具生成的ncc配置文件" << endl;
+	getline(cin, filePath);
+	if (filePath[0] == '"') filePath = filePath.substr(1, filePath.length() - 2);
+	if (filePath.substr(filePath.length() - 3, 3) == "ncc") {
+		ifstream infile;
+		string picPath;
+		infile.open(filePath);
+		infile >> picPath;
+		Mat dst = imread(picPath, -1);
+		chann = dst.channels();
+		vector<Point> ROIArea;
+		int x, y;
+		infile >> x;
+		while (x != -1) {
+			infile >> y;
+			ROIArea.emplace_back(Point(x, y));
+			infile >> x;
+		}
+		system("mkdir final");
+		clearr(dst, ROIArea);
+		myRGB(dst, ROIArea, 2, 1);
+		myRGB(dst, ROIArea, 1, 0);
+		myRGB(dst, ROIArea, 0, 2);
+		system("ffmpeg -i final\\final%d.png -vf palettegen palette.png -y");
+		system("ffmpeg -thread_queue_size 96 -i final\\final%d.png -i palette.png -filter_complex \"[0:v]setpts=0.05*PTS[v];[v][1:v]paletteuse\" final.gif -y");
+		system("rd /s /q final");
+		system("del palette.png");
+		system("ren final.gif %date:~0,4%_%date:~5,2%_%date:~8,2%_%time:~6,2%_%time:~9,2%.gif");
+		cout << "完成" << endl;
+		system("ping -n 1 127.0.0.1>nul");
+		return 0;
+	}
+
+
+	src = imread(filePath, -1);
+	chann = src.channels();
+
+	do {
+		cout << "1黑底2白底" << endl;
+		cin >> a;
+		if (a == 1) {
+			dst = Mat::zeros(src.rows, src.cols, src.type());
+		} else if (a == 2) {
+			dst = Mat(src.rows, src.cols, src.type(), Scalar(255, 255, 255));
+		}
+	} while (a != 1 && a != 2);
+
+	cvtColor(src, src, COLOR_BGR2GRAY);
+	sequ.emplace_back(src);
+
+	while (1) {
+		system("cls");
+		destroyAllWindows();
+		cout << "提示:不清楚功能请勿擅用 建议百度" << endl;
+		cout << "包含文字或图片较小建议直接二值化然后ok" << endl;
+		cout << "输入序号选择要进行的步骤" << endl << endl;
+		cout << "1.二值化 将灰度图转换成只有纯黑和纯白的图像" << endl;
+		cout << "2.中值滤波 用来除去斑点噪声和椒盐噪声" << endl;
+		cout << "3.腐蚀 可以认为是将黑色区域扩大" << endl;
+		cout << "4.膨胀 可以认为是将白色区域扩大" << endl;
+		cout << "5.开操作 可以消除白色的小点 去除小的干扰块" << endl;
+		cout << "6.闭操作 可以消除黑色的小块 填充闭合区域" << endl;
+		cout << "7.梯度 就是膨胀和腐蚀的差 可用来保留轮廓" << endl;
+		cout << "8.Canny边缘检测" << endl;
+		
+		cout << "输入yl来预览当前图像" << endl;
+		cout << endl << "输入fh数字进行反悔" << endl;
+		cout << "例如fh6返回到第6步" << endl;
+
+		cout << endl << "输入ok退出图像操作" << endl;
+		cin >> n;
+		if (n.substr(0, 2) == "fh") {
+			while (sequ.size() != (stoi(n.substr(2)) + size_t(1))) sequ.pop_back();
+			continue;
+		}
+		if (n == "yl") {
+			cout << "将焦点放到预览图上 按任意键退出预览" << endl;
+			imshow("预览", sequ.back());
+			waitKey(0);
+			continue;
+		}
+
+		if (n == "1") {
+			shold();
+		} else if (n == "2") {
+			median();
+		} else if (n == "3") {
+			erosion();
+		} else if (n == "4") {
+			dilation();
+		} else if (n == "5") {
+			open();
+		} else if (n == "6") {
+			close();
+		} else if (n == "7") {
+			gradient();
+		} else if (n == "8") {
+			canny();
+		} else if (n == "ok") break;
+	}
+
+
+	dst_src = sequ.back();
+	destroyAllWindows();
+	for (register int i = 0; i < dst_src.rows; i++) {
+		for (register int j = 0; j < dst_src.cols; j++) {
+			if (!dst_src.at<uchar>(i, j)) {
+				vec.emplace_back(Point(i, j));
+			}
+		}
+	}
+
+	system("mkdir final");
+	clearr(dst, vec);
+	myRGB(dst, vec, 2, 1);
+	myRGB(dst, vec, 1, 0);
+	myRGB(dst, vec, 0, 2);
+	system("ffmpeg -i final\\final%d.png -vf palettegen palette.png -y");
+	system("ffmpeg -thread_queue_size 96 -i final\\final%d.png -i palette.png -filter_complex \"[0:v]setpts=0.05*PTS[v];[v][1:v]paletteuse\" final.gif -y");
+	system("rd /s /q final");
+	system("del palette.png");
+	system("ren final.gif %date:~0,4%_%date:~5,2%_%date:~8,2%_%time:~6,2%_%time:~9,2%.gif");
+	cout << "完成" << endl;
+	system("ping -n 1 127.0.0.1>nul");
+	return 0;
+}
---
+++ 魔棒(相似区域选择工具)_1_0_2.cpp
@@ -0,0 +1,166 @@
+﻿#include<iostream>
+#include<fstream>
+#include<queue>
+#include<vector>
+#include<string>
+#include<thread>
+#include<cstdio>
+#include<opencv2/opencv.hpp>
+//CODER BY Vistyxio POWERD BY VS2019
+using namespace std;
+using namespace cv;
+
+int MagicThreshold = 15;
+int f, chann;
+Mat src, src_gray;
+string filePath, temp, str;
+vector<Point2d> ROIArea;
+
+inline bool check(int org_x, int org_y, int x, int y, bool** flag) {
+	if (x < 0 || y < 0 || x >= src.rows || y >= src.cols) return 0;
+	if (abs(src_gray.at<uchar>(org_x, org_y) - src_gray.at<uchar>(x, y)) > MagicThreshold) return 0;
+	if (flag[x][y] == 1) return 0;
+	return 1;
+}
+
+string getFileName(string filePath) {
+	string str;
+	for (register long long i = filePath.length() - 1; i >= 0; i--) {
+		if (filePath[i] == '\\') break;
+		str += filePath[i];
+	}
+	reverse(str.begin(), str.end());
+	return str;
+}
+
+void writeDAT(vector<Point2d> ROIArea) {
+	ofstream outfile;
+	if (f == 1) {
+		outfile.open(temp, ios::out);
+		outfile << filePath << endl;
+	} else {
+		outfile.open(temp, ios::out | ios::app);
+	}
+	for (auto i : ROIArea) {
+		outfile << i.x << " " << i.y << endl;
+	}
+	outfile.close();
+	cout << "配置文件写入完成" << endl;
+}
+
+void MagicWandTool(int org_x, int org_y) {
+	Mat src_magic;
+	src.copyTo(src_magic);
+
+	int dir[4][2] = { 0,1,1,0,0,-1,-1,0 };
+	bool** flag = new bool* [src.rows];
+	for (register size_t i = 0; i < src.rows; i++) flag[i] = new bool[src.cols];
+	queue<Point2d> MagicArea;
+	
+	MagicArea.push(Point2d(org_x, org_y));
+	ROIArea.emplace_back(Point2d(org_x, org_y));
+	flag[org_x][org_y] = 1;
+	while (!MagicArea.empty()) {
+		int now_x = MagicArea.front().x;
+		int now_y = MagicArea.front().y;
+		MagicArea.pop();
+		
+		for (register size_t i = 0; i < 4; i++) {
+			int xx = now_x + dir[i][0];
+			int yy = now_y + dir[i][1];
+			if (check(org_x, org_y, xx, yy, flag)) {
+				MagicArea.push(Point2d(xx, yy));
+				ROIArea.emplace_back(Point2d(xx, yy));
+				flag[xx][yy] = 1;
+			}
+		}
+	}
+
+	writeDAT(ROIArea);
+	bool num = 0;
+	for (auto i : ROIArea) {
+		if (!num) {
+			num = 1;
+			if (chann == 4) {
+				src_magic.at<Vec4b>(i.x, i.y)[0] = 80;
+				src_magic.at<Vec4b>(i.x, i.y)[1] = 80;
+				src_magic.at<Vec4b>(i.x, i.y)[2] = 80;
+			} else {
+				src_magic.at<Vec3b>(i.x, i.y)[0] = 80;
+				src_magic.at<Vec3b>(i.x, i.y)[1] = 80;
+				src_magic.at<Vec3b>(i.x, i.y)[2] = 80;
+			}
+			
+		} else {
+			num = 0;
+			if (chann == 4) {
+				src_magic.at<Vec4b>(i.x, i.y)[0] = 160;
+				src_magic.at<Vec4b>(i.x, i.y)[1] = 160;
+				src_magic.at<Vec4b>(i.x, i.y)[2] = 160;
+			} else {
+				src_magic.at<Vec3b>(i.x, i.y)[0] = 160;
+				src_magic.at<Vec3b>(i.x, i.y)[1] = 160;
+				src_magic.at<Vec3b>(i.x, i.y)[2] = 160;
+			}
+		}
+
+	}
+	if(f == 1) ROIArea.clear();
+
+	imshow("魔棒", src_magic);
+	return;
+}
+
+
+
+void OnMouse(int event, int x, int y, int flags, void*) {
+	if (event == EVENT_LBUTTONDOWN) {
+		//需要注意的是，窗口横轴为X轴，竖轴为Y轴，坐标是(x, y)，而矩阵坐标是按照(row, col)，故窗口中的点(x, y)对应的值在矩阵中为(y, x)。
+		if (y >= 0 && x >= 0 && y < src.rows && x < src.cols) MagicWandTool(y, x);
+	}
+}
+
+int main() {
+	
+	cout << "欢迎使用魔棒工具" << endl;
+	cout << "本工具类似Ps的魔棒 可以选取类似区域" << endl;
+	cout << "本魔棒工具需要配合1.1.2版本以上的图片RGB化使用" << endl;
+	cout << "输入路径或拖动图片到此" << endl;
+	getline(cin, filePath);
+	if (filePath[0] == '"') filePath = filePath.substr(1, filePath.length() - 2);
+	temp = getFileName(filePath);
+	replace(temp.begin(), temp.end(), '.', '_');
+	temp += ".ncc";
+	fstream outfile;
+	outfile.open(temp, ios::out | ios::app);
+	outfile << filePath << endl;
+	src = imread(filePath, -1);
+	chann = src.channels();
+	cvtColor(src, src_gray, COLOR_BGR2GRAY);
+	cout << "输入魔棒阈值 当其他点的灰度值与选定点的灰度值的差的绝对值大于阈值时忽略 默认15" << endl;
+	cout << "通俗来说就是魔棒阈值越大选取的范围越大" << endl;
+	cin >> MagicThreshold;
+	cout << "输入1 选择单次选取模式" << endl;
+	cout << "输入2 选择多次选取模式" << endl;
+	cin >> f;
+	cout << endl << "左键点击进行选取" << endl;
+	cout << "在选取魔棒区域时程序会生成配置文件" << endl;
+	cout << "在确定魔棒区域后将焦点放于图像窗口上按任意键退出" << endl;
+	cout << "可在本程序文件夹下找到配置文件" << endl;
+
+	imshow("魔棒", src);
+	setMouseCallback("魔棒", OnMouse);
+	
+	waitKey(0);
+	
+	outfile << "-1";
+	outfile.close();
+	time_t now = time(NULL);
+	str = temp.substr(0, temp.length() - 4);
+	str += '_';
+	str += to_string(now);
+	str += ".ncc";
+	char c = rename(temp.c_str(), str.c_str());
+	cout << "完成！！！按任意键退出" << endl;
+	return 0;
+}
